cec6f8ca0215114e111fa5b9e73823d522b70931
Virayut Sandhu <virayuts@gmail.com>
2021-07-07 15:00:11 +0700

    gcp compute engine and vpc

--- cloud/gcp/00-notes.sh
+++ cloud/gcp/00-notes.sh
@@ -122,3 +122,96 @@
 #   as needed before launch
 # - Once deployed, if the base image of package gets updated, it doesn't affect the already deployed instances and has
 #   to be done manually
+
+# Compute Engine
+# - It lets you configure and run virtual machine on GCP
+# - It lets you configure vCPUs, memory, GPUs, storage disks, image (OS and softwares), etc.
+# - It also lets you take disk snapshot of VM for backup, migration, etc.
+
+# Compute Engine Option
+# - VM machine type is pre-defined but it can be customised by configuring desired vCPUs, memory, etc., combination
+#   to suite your application needs
+# - VM metadata can be associated for describing itself which is in the form of key-value pair
+# - VM storage disk type includes
+#   - Persistent disk
+#       - It persists data when VM terminates
+#       - It is a network storage
+#   - Local SSD
+#       - It doesn't persist data when VM terminates
+# - VM image is avaialble to choose from or your own image can be supplied as well
+#   - Startup script can also be provided to execute on VM startup
+
+# Creating a VM
+# - VM_NAME is a hostname which can be used to connect to within the network
+#   - Its full format is VM_NAME.ZONE_NAME.c.PROJECT_ID.internal
+gcloud compute zones list                   # List all the available zones for compute engine
+gcloud config set compute/zone ZONE_NAME    # Set zone to work with
+gcloud compute instances create VM_NAME     # Creating a VM (many command options exist to tune VM)
+
+# Virtual Private Cloud (VPC)
+# - It helps with GCP resources connectivity
+#   - Connect them with each other or to the internet
+#   - Isolate them from one another
+# - It provides
+#   - Segment network
+#       - Subnets can be in any region worldwide
+#       - Subnets can span multiple zones in a region
+#       - Subnets can be increased dynamically without affecting the already configured virtual machines
+#   - Firewall rules to restrict traffic
+#       - It control incoming and outgoing traffic
+#       - It can uses metadata tag from VM to construct rule
+#   - Forward traffic
+#       - It uses a route table to forward traffic within a network, across subnet, between zones
+# - VPC network is contained in a GCP project
+#   - For connecting to another network within GCP
+#       - VPC Peering
+#           - It helps with inter-connecting VPC network in another project
+#       - Shared VPC
+#           - It helps with sharing a network or individual subnets with another project
+#           - It integrates with IAM to control access
+#   - For connection to another network outside of GCP
+#       - VPN
+#           - It helps connect over the internet using IPsec protocol
+#           - It uses Cloud Router to exchange routes between GCP network and other network using
+#             Border Gateway Protocol, e.g., by adding a new subnet in GCP, the other network will get route to it
+#             automatically
+#       - Direct Peering
+#           - Private connection between GCP and other network
+#           - It works by putting router in the same public data center as Google point of presence to exchange
+#             traffic
+#       - Carrier Peering
+#           - It is similiar to direct peering but connection is provided through partner who is already present in the
+#             public data center
+#       - Dedicated Interconnect
+#           - It is a direct private connection to Google with guarantee of up to 99.9% SLA if network topologies meet
+#             specification
+
+# Cloud Load Balancing
+# - It is a fully distributed, managed service that helps with distributing incoming traffic from the internet 
+#   to internal back-end services
+# - It provides a single anycast IP address (many machine having same IP address) to front end all the request from
+#   any region
+# - It selects traffic based on back-end health, network condition, etc.
+# - It has several type of load balancer
+#   - Global HTTP(s)
+#       - It load balanced against Layer 7 HTTP(s) protocols based on URL
+#   - Global SSL Proxy
+#       - It load balanced against Layer 4 TCP (any non-http) with SSL protocols
+#       - It works only on specific port number
+#   - Global TCP Proxy
+#       - It load balanced against Layer 4 TCP (any-non-http) with no SSL protocols
+#       - It works only on specific port number
+#   - Regional
+#       - It load balanced against Layer 4 TCP or UDP
+#       - It works on any port number
+#   - Regional Internal
+#       - It load balanced against traffic within the GCP
+#       - It accepts request on internal GCP IP address
+
+# Cloud DNS
+# - It helps translate internet hostnames to IP addresses of applications within GCP
+#   - GCP automatically provides Domain Name Service resolution for internal IP addresses of VM instances
+
+# Cloud CDN (Content Delivery Network)
+# - It helps cache content in Google's edge caches closer to end user
+# - CDN Interconnect allows you to use different CDN
